6c555a57683900a8a29bdff670040a63d1c73107
ElianGalindo <elianalejandro202@gmail.com>
2024-03-17 19:22:58 -0600

    Arreglos

--- components/twitter/ui-tweet.vue
+++ components/twitter/ui-tweet.vue
@@ -1,51 +1,49 @@
 <template>
   <v-row class="ma-1 pa-1">
-    <v-row style="margin:-7px;">
-      <v-col>
-        <v-img
-          :src="require('@/assets/avatars/my-avatar.svg')"
-          style="border-radius: 50%;"
-        >
-        </v-img>
-      </v-col>
-      <v-col cols="10" class="ma-1 pa-1 mb-3">
-        <v-row>
-          <v-textarea
-            v-model="tweet"
-            rows="2"
-            row-height="5"
-            :rules="countTweet"
-          />
-        </v-row>
-        <v-row>
-          <v-col cols="1">
-            <v-img :src="require('@/assets/icons/imagen.svg')"></v-img>
-          </v-col>
-          <v-col cols="1">
-            <v-img :src="require('@/assets/icons/gift.svg')"></v-img>
-          </v-col>
-          <v-col cols="1">
-            <v-img :src="require('@/assets/icons/estadisticas.svg')"></v-img>
-          </v-col>
-          <v-col cols="1">
-            <v-img :src="require('@/assets/icons/reaccion.svg')"></v-img>
-          </v-col>
-          <v-col cols="1">
-            <v-img :src="require('@/assets/icons/calendario.svg')"></v-img>
-          </v-col>
-          <v-col>
-            <v-row align="center" justify="end">
-              <v-btn rounded color="primary">
-                <span style="text-transform: none;">
-                  tweet
-                </span>
-              </v-btn>
-            </v-row>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-row class="ma-2 pa-2 mb-3 my-0">
+    <v-col>
+      <v-img
+        :src="require('@/assets/avatars/my-avatar.svg')"
+        style="border-radius: 50%;"
+      >
+      </v-img>
+    </v-col>
+    <v-col cols="10" class="ma-1 pa-1 mb-3">
+      <v-row>
+        <v-textarea
+          v-model="tweet"
+          rows="2"
+          row-height="5"
+          :rules="countTweet"
+        />
+      </v-row>
+      <v-row>
+        <v-col cols="1">
+          <v-img :src="require('@/assets/icons/imagen.svg')"></v-img>
+        </v-col>
+        <v-col cols="1">
+          <v-img :src="require('@/assets/icons/gift.svg')"></v-img>
+        </v-col>
+        <v-col cols="1">
+          <v-img :src="require('@/assets/icons/estadisticas.svg')"></v-img>
+        </v-col>
+        <v-col cols="1">
+          <v-img :src="require('@/assets/icons/reaccion.svg')"></v-img>
+        </v-col>
+        <v-col cols="1">
+          <v-img :src="require('@/assets/icons/calendario.svg')"></v-img>
+        </v-col>
+        <v-col>
+          <v-row align="center" justify="end">
+            <v-btn rounded color="primary">
+              <span style="text-transform: none;">
+                tweet
+              </span>
+            </v-btn>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-col>
+    <v-row class="ma-2 pa-2 mb-3">
       <ui-tweets />
     </v-row>
   </v-row>
